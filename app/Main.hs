364b7e81c5288b0ce12dacef43c61f6c8b0e4ca7
Pratap Singh <pratapsingh1729@gmail.com>
2022-01-24 22:25:39 -0500

    first impl of wordle pass

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,25 @@
 module Main where
 
-import Lib
+import qualified Data.Set as Set
+
+goal :: String
+goal = "raise"
+
+vocab :: [String]
+vocab = ["raise", "tough", "royal", "spine"]
+
+iter :: String -> String
+iter guess
+  | guess `elem` vocab = map (checkLetter goal) $ zip goal guess
+  | otherwise = "Not a valid guess!"
+  where
+    checkLetter goal (goalCh, guessCh)
+      | goalCh == guessCh = 'X'
+      | guessCh `elem` goal = 'x'
+      | otherwise = '_'
 
 main :: IO ()
-main = someFunc
+main = do
+  guess <- getLine
+  putStrLn $ iter guess
+  return ()
