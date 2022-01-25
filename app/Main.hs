a083d00af02e5bf9593ec891c806ec021526b941
Pratap Singh <pratapsingh1729@gmail.com>
2022-01-24 23:41:16 -0500

    looping works

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,17 @@
 module Main where
 
 import qualified Data.Set as Set
+import System.IO (hFlush, stdout)
 
 goal :: String
 goal = "knoll"
 
 vocab :: Set.Set String
-vocab = Set.fromList ["raise", "tough", "royal", "spine", "klick", "droll", "loyal"]
+vocab = Set.fromList ["raise", "tough", "royal", "spine", "klick", "droll", "loyal", "knoll"]
 
 checkValidGuess :: String -> String -> String
 checkValidGuess goal guess = go (Set.fromList goal) goal guess
-  where 
+  where
     go goalChars (goalC : goalT) (guessC : guessT)
       | goalC == guessC = 'X' : go (Set.delete guessC goalChars) goalT guessT
       | guessC `elem` goalChars = 'x' : go (Set.delete guessC goalChars) goalT guessT
@@ -18,15 +19,22 @@ checkValidGuess goal guess = go (Set.fromList goal) goal guess
     go _ [] [] = ""
     go _ _ _ = "Err: non-5-letter-word " ++ guess ++ " found in vocab"
 
-
-checkGuess :: String -> String -> String
-checkGuess goal guess
+checkGuess :: Set.Set String -> String -> String -> String
+checkGuess vocab goal guess
   | guess == goal = "You win!"
   | guess `elem` vocab = checkValidGuess goal guess
   | otherwise = "Not a valid guess!"
 
+loop :: Integer -> IO ()
+loop n = 
+  if n > 6
+    then do putStrLn "Out of guesses!"
+            return ()
+    else do putStr $ "Guess " ++ show n ++ ": "
+            hFlush stdout
+            guess <- getLine
+            putStrLn $ " Result: " ++ checkGuess vocab goal guess
+            loop (n + 1)
+
 main :: IO ()
-main = do
-  guess <- getLine
-  putStrLn $ checkGuess goal guess
-  return ()
+main = loop 1
